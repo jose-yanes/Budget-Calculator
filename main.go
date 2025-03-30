d0a60a81bfccc11bf4efae299fe80626ac8ea387
Jose Yanes <113186145+jose-yanes@users.noreply.github.com>
2025-03-29 21:17:14 -0300

    Adding loop and dialog to continue

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"github.com/dustin/go-humanize"
 	"log"
 	"os"
 	"strconv"
+
+	"github.com/dustin/go-humanize"
 )
 
 func main() {
@@ -15,50 +16,63 @@ func main() {
 ██   ██ ██    ██ ██   ██ ██       ██         ██        ██      ██      ██   ██ ██      ██   ██ ██    ██ ██      ██      ██   ██ 
 ██████  ██    ██ ██   ██ ██   ███ █████      ██        ███████ ██      ███████ █████   ██   ██ ██    ██ ██      █████   ██████  
 ██   ██ ██    ██ ██   ██ ██    ██ ██         ██             ██ ██      ██   ██ ██      ██   ██ ██    ██ ██      ██      ██   ██ 
-██████   ██████  ██████   ██████  ███████    ██        ███████  ██████ ██   ██ ███████ ██████   ██████  ███████ ███████ ██   ██
-`)
+██████   ██████  ██████   ██████  ███████    ██        ███████  ██████ ██   ██ ███████ ██████   ██████  ███████ ███████ ██   ██`)
 
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Select the percentages that you want to use")
-	fmt.Println("Percentage for Necessities: ")
+	for {
+		fmt.Println("Select the percentages that you want to use")
+		fmt.Println("Percentage for Necessities: ")
 
-	necAmt, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
+		necAmt, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	fmt.Println("Percentage for Wants: ")
-	wantsAmt, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
+		fmt.Println("Percentage for Wants: ")
+		wantsAmt, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	fmt.Println("Percentage for Savings / Debt: ")
-	savingsAmt, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
+		fmt.Println("Percentage for Savings / Debt: ")
+		savingsAmt, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	fmt.Println("Income: ")
-	incomeAmt, err := reader.ReadString('\n')
+		fmt.Println("Income: ")
+		incomeAmt, err := reader.ReadString('\n')
 
-	necAmtInt, err := strconv.Atoi(necAmt[:len(necAmt)-1])
-	wantsAmtInt, err := strconv.Atoi(wantsAmt[:len(wantsAmt)-1])
-	savingsAmtInt, err := strconv.Atoi(savingsAmt[:len(savingsAmt)-1])
-	incomeAmtInt, err := strconv.Atoi(incomeAmt[:len(incomeAmt)-1])
+		necAmtInt, err := strconv.Atoi(necAmt[:len(necAmt)-1])
+		wantsAmtInt, err := strconv.Atoi(wantsAmt[:len(wantsAmt)-1])
+		savingsAmtInt, err := strconv.Atoi(savingsAmt[:len(savingsAmt)-1])
+		incomeAmtInt, err := strconv.Atoi(incomeAmt[:len(incomeAmt)-1])
 
-	necPer, wantPer, savPer := makeCalculation(necAmtInt, wantsAmtInt, savingsAmtInt, incomeAmtInt)
+		necPer, wantPer, savPer := makeCalculation(necAmtInt, wantsAmtInt, savingsAmtInt, incomeAmtInt)
 
-	fmt.Println("Your percentages are: ")
+		fmt.Println("Your percentages are: ")
 
-	fmt.Printf(`
+		fmt.Printf(`
 	Necessities: %s,
 	Wants: %s,
 	Savings / Debt: %s,
-    Income: %s,\n\n`, humanize.Commaf(necPer), humanize.Commaf(wantPer), humanize.Commaf(savPer), humanize.Comma(int64(incomeAmtInt)))
+    Income: %s,
+`, humanize.Commaf(necPer), humanize.Commaf(wantPer), humanize.Commaf(savPer), humanize.Comma(int64(incomeAmtInt)))
 
-	if err != nil {
-		log.Fatal(err)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Do you want to calculate a different income? (y/n)")
+		restartProgram, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		if restartProgram[:1] != "y" {
+			println("Have a nice day :)")
+			return
+		}
+
 	}
 
 }
